c3b1ededfa8302a29f54dd10b57db634c2317532
Mitch Dzugan <mitch.dzugan@everymove.com>
2015-10-24 13:44:37 -0700

    Add ability to hover over dataset lines

--- client/src/main/scala/spatutorial/client/components/Chart.scala
+++ client/src/main/scala/spatutorial/client/components/Chart.scala
@@ -11,12 +11,13 @@ import scala.scalajs.js.annotation.JSName
 trait ChartDataset extends js.Object {
   def label: String = js.native
   def fillColor: String = js.native
-  def strokeColor: String = js.native
+  var strokeColor: String = js.native
   def data: js.Array[Double] = js.native
+  def points : js.Array[Helpers.Position] = js.native
 }
 
 object ChartDataset {
-  def apply(data: Seq[Double], label: String, fillColor: String = "#8080FF", strokeColor: String = "#404080"): ChartDataset = {
+  def apply(data: Seq[Double], label: String, fillColor: String = "#8080FF", strokeColor: String = "rgba(64,64,128,0.5)"): ChartDataset = {
     js.Dynamic.literal(
       data = data.toJSArray,
       label = label,
@@ -40,12 +41,30 @@ object ChartData {
   }
 }
 
+@JSName("Chart.helpers")
+object Helpers extends js.Object {
+  trait Event extends js.Object
+  trait Position extends js.Object {
+    val x : Double
+    val y : Double
+  }
+  def addEvent(canvas : HTMLCanvasElement, name: String, callback: js.Function1[Event, Unit]) = js.native
+  def getRelativePosition(e: Event) : Position = js.native
+}
+
+trait LineChart extends js.Object {
+  val datasets : js.Array[ChartDataset] = js.native
+  def eachPoints(callback: js.Function1[Helpers.Position, Unit]) = js.native
+  var bezierCurve : Boolean = js.native
+  val draw : js.ThisFunction0[LineChart, Unit] = js.native
+}
+
 // define a class to access the Chart.js component
 @JSName("Chart")
 class JSChart(ctx: js.Dynamic) extends js.Object {
   // create different kinds of charts
-  def Line(data: ChartData): js.Dynamic = js.native
-  def Bar(data: ChartData): js.Dynamic = js.native
+  def Line(data: ChartData, options: js.Object): LineChart = js.native
+  def Bar(data: ChartData): LineChart = js.native
 }
 
 object Chart {
@@ -57,7 +76,7 @@ object Chart {
 
   case object BarChart extends ChartStyle
 
-  case class ChartProps(name: String, style: ChartStyle, data: ChartData, width: Int = 400, height: Int = 200)
+  case class ChartProps(name: String, style: ChartStyle, data: ChartData, width: Int = 400, height: Int = 200, additionalSetup: (HTMLCanvasElement, LineChart) => Unit)
 
   class Backend(t: BackendScope[ChartProps, _])
 
@@ -66,13 +85,20 @@ object Chart {
     <.canvas(^.width := P.width, ^.height := P.height)
   }).componentDidMount(scope => {
     // access context of the canvas
-    val ctx = scope.getDOMNode().asInstanceOf[HTMLCanvasElement].getContext("2d")
+    val canvas = scope.getDOMNode().asInstanceOf[HTMLCanvasElement]
+    val ctx = canvas.getContext("2d")
     // create the actual chart using the 3rd party component
-    scope.props.style match {
-      case LineChart => new JSChart(ctx).Line(scope.props.data)
+    val c = scope.props.style match {
+      case LineChart => new JSChart(ctx).Line(scope.props.data, js.Dynamic.literal(
+        bezierCurve = false,
+        showTooltips = false
+      ))
       case BarChart => new JSChart(ctx).Bar(scope.props.data)
       case _ => throw new IllegalArgumentException
     }
+
+    scope.props.additionalSetup(canvas, c)
+
   }).build
 
   def apply(props: ChartProps) = Chart(props)
--- client/src/main/scala/spatutorial/client/modules/Dashboard.scala
+++ client/src/main/scala/spatutorial/client/modules/Dashboard.scala
@@ -1,24 +1,85 @@
 package spatutorial.client.modules
 
-import japgolly.scalajs.react.ReactComponentB
+import japgolly.scalajs.react.{BackendScope, ReactComponentB}
 import japgolly.scalajs.react.extra.router2.RouterCtl
 import japgolly.scalajs.react.vdom.prefix_<^._
 import spatutorial.client.SPAMain.{TodoLoc, Loc}
 import spatutorial.client.components._
 
 object Dashboard {
+  case class State(index : Option[Int] = None, prevIndex : Option[Int] = None)
+
+  class Backend(t: BackendScope[RouterCtl[Loc], State]) {
+    def newInd(lc: LineChart,index: Option[Int]): Unit = {
+      if (t.state.index != index) {
+        t.modState(curr => State(index, curr.index))
+        t.state.index match {
+          case Some(i) => lc.datasets(i).strokeColor = "rgba(64,64,128,1)"
+          case _ => ()
+        }
+        t.state.prevIndex match {
+          case Some(i) => lc.datasets(i).strokeColor = "rgba(64,64,128,0.5)"
+          case _ => ()
+        }
+        lc.draw(lc)
+      }
+    }
+  }
+
   // create the React component for Dashboard
   val component = ReactComponentB[RouterCtl[Loc]]("Dashboard")
-    .render(ctl => {
-    // create dummy data for the chart
-    val cp = Chart.ChartProps("Test chart", Chart.BarChart, ChartData(Seq("A", "B", "C"), Seq(ChartDataset(Seq(1, 2, 3), "Data1"))))
-    <.div(
-      // header, MessageOfTheDay and chart components
-      <.h2("Dashboard"),
-      Motd(),
-      Chart(cp),
-      // create a link to the Todo view
-      <.div(ctl.link(TodoLoc)("Check your todos!"))
-    )
-  }).build
+    .initialState(State())
+    .backend(new Backend(_))
+    .render((ctl, S, B) => {
+      // create dummy data for the chart
+      val cp = Chart.ChartProps(
+        "Test chart",
+        Chart.LineChart,
+        ChartData(Seq("A", "B", "C"),
+          Seq(
+            ChartDataset(Seq(6, 8, 3), "Data1", "rgba(220,220,220,0)"),
+            ChartDataset(Seq(1, 2, 7), "Data2", "rgba(220,220,220,0)")
+          )),
+        additionalSetup = (canvas, lc) => {
+          try {
+            Helpers.addEvent(canvas, "mousemove", (event: Helpers.Event) => {
+              val mouse = Helpers.getRelativePosition(event)
+              val min = lc.datasets.zipWithIndex.flatMap(t => {
+                var prev : Option[Helpers.Position] = None
+                var curr : Option[Helpers.Position] = None
+                (t._1.points.to[List].dropWhile(point => {
+                  prev = curr
+                  curr = Some(point)
+                  point.x < mouse.x
+                }), prev) match {
+                  case (Nil, _) => None
+                  case (_, None) => None
+                  case (rightPoint::_, Some(leftPoint)) =>
+                    Some(t._2, Math.abs(
+                      leftPoint.y - mouse.y + (
+                        (mouse.x - leftPoint.x) * (rightPoint.y - leftPoint.y) /
+                          (rightPoint.x - leftPoint.x)
+                        )))
+                }
+              }).reduceOption(Ordering.by((_ : (Int, Double))._2).min) match {
+                case Some((ind, distance)) if distance < 5.0 => Some(ind)
+                case _ => None
+              }
+              B.newInd(lc, min)
+            })
+          } catch {
+            case e => println(e)
+          }
+        }
+      )
+      <.div(
+        // header, MessageOfTheDay and chart components
+        <.h2("Dashboard"),
+        Motd(),
+        Chart(cp),
+        // create a link to the Todo view
+        <.h2(S.index),
+        <.div(ctl.link(TodoLoc)("Check your todos!"))
+      )
+    }).build
 }
